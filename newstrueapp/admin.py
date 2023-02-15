21d53a139a722629c771e8ec5469e6cb4d79f89e
MarinaProsche <120782374+MarinaProsche@users.noreply.github.com>
2023-02-15 12:03:20 +0300

    Delete newstrueapp directory

--- newstrueapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Author, Post, Cathegory, Comment, PostCathegory, SubscribersCathegory
-# Register your models here.
-admin.site.register(Author)
-admin.site.register(Post)
-admin.site.register(Cathegory)
-admin.site.register(Comment)
-admin.site.register(PostCathegory)
-admin.site.register(SubscribersCathegory)
