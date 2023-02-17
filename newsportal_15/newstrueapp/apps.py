9fcddd1b6e207ed2b160c8981c3df79f40dad183
MarinaProsche <120782374+MarinaProsche@users.noreply.github.com>
2023-02-17 19:31:34 +0300

    Delete newsportal_15 directory

--- newsportal_15/newstrueapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewstrueappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'newstrueapp'
-    def ready(self):
-        import newstrueapp.signals
